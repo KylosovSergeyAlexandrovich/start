b4705ae542f378f6b1d1bd1ed4017a50cb0817f3
KylosovSergeyAlexandrovich <formozaiti@gmail.com>
2020-02-22 18:41:13 +0300

    start3_session

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,10 +11,21 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 class HomePageController extends AbstractController
 {
+
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+
+
 
     /**
     * @Route("/")
@@ -23,9 +34,25 @@ class HomePageController extends AbstractController
     {
         $number = random_int(0, 100);
 
+
+
+        // stores an attribute in the session for later reuse
+        $this->session->set('auth', 'on');
+
+        // gets an attribute by name
+        $auth = $this->session->get('auth');
+
+        // the second argument is the value returned when the attribute doesn't exist
+        $filters = $this->session->get('filters', []);
+
+
+
+
+
         
         return $this->render('HomePage.html.twig', [
             'number' => $number,
+            'auth' => $auth,
         ]);
     }
 }
